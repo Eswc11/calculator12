419b2e8d2af19d458f6c99f5b832e6e9610ba0f4
Eswc <yyf.eswc@bk.ru>
2025-10-08 15:36:19 +0600

    r

---
+++ script.py
@@ -0,0 +1,13 @@
+a = input("первое число: ")
+b = input("второе число: ")
+c = input("Операция: ")
+if c == "+":
+    s = a + b
+    print(s)
+elif c == "-":
+    s = a - b
+    print(s)
+elif c == "*":
+    print(a * b)
+elif c == "/":
+    print(a / b)
