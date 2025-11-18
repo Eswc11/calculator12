434b7b6c4a17f10a1cd6199ca9504d845a6fa997
Eswc <yyf.eswc@bk.ru>
2025-11-18 21:06:49 +0600

    a

--- hw2.py
+++ hw2.py
@@ -27,34 +27,49 @@ def check_probability():
 def robot_damage():
     hit_probability = random.randint(1, 3)
     if hit_probability == 1:
-        damage = robot['gun'] * 0,3 - hero['defence']
-        modify_health(hero, damage)
+        damage = robot['gun'] + 0.3 * robot['gun'] - hero['defence']
+        if damage <= 0:
+            return 'Damage is weak.Hero does not take damage.'
+        else:
+            modify_health(hero, -damage)
+            return f'Hero take {damage} damage'
     if hit_probability == 2:
         chance = random.randint(1, 2)
         if chance == 1:
             damage = robot['gun'] - hero['defence']
-            modify_health(hero, damage)
+            if damage <= 0:
+                return 'Damage is weak.Robot does not take damage.'
+            else:
+                modify_health(hero,-damage)
+                return f'Hero take {damage} damage'
         else:
             return 'Robot missed!'
     else:
         return 'Robot is not worked'
 def display_hero_info():
-    return f'Hero is {hero["hp"]} HP.'
+    return f'Hero has {hero["hp"]} HP.'
 def display_robot_info():
     return f'Robot has {robot["hp"]} HP.'
 def step_hero():
-    check_probability()
-    display_hero_info()
+    print(check_probability())
+    print(display_robot_info())
 def step_robot():
-    robot_damage()
-    display_robot_info()
+    print(robot_damage())
+    print(display_hero_info())
 def modify_health(person,hp_change):
     person['hp'] += hp_change
     if person['hp'] <= 0:
         person['hp'] = 0
 def main():
-    while robot['hp'] > 0:
-        input("Press Enter to continue to next step...")
+    while robot['hp'] > 0 and hero['hp'] > 0:
+        input("Press Enter to hero step...")
         step_hero()
-    print('Robot is dead')
+        if robot['hp'] <= 0:
+            print('Robot is dead')
+            break
+        input("Press Enter to robot step...")
+        step_robot()
+        if hero['hp'] <= 0:
+            print('Hero is dead')
+            break
 main()
